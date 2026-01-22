e761e36085f673093114fa50a9d1db6eccc04a01
Falesz <jnjwrg@inf.elte.hu>
2026-01-22 09:41:28 +0100

    Add comments to Note and Interval enums to indicate the indices of their elements

--- Note.js
+++ Note.js
@@ -1,11 +1,11 @@
 const RootNotesEnum = {
-    A: 'A',
-    B: 'B',
-    C: 'C',
-    D: 'D',
-    E: 'E',
-    F: 'F',
-    G: 'G'
+    A: 'A', // 0
+    B: 'B', // 1
+    C: 'C', // 2
+    D: 'D', // 3
+    E: 'E', // 4
+    F: 'F', // 5
+    G: 'G'  // 6
 };
 
 const ModificationType = {

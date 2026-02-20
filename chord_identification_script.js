90f85d4beaed3ee3bce8445da386d30d94d02f24
Falesz <jnjwrg@inf.elte.hu>
2026-02-20 09:20:38 +0100

    Add more document variables to chord identification script

--- chord_identification_script.js
+++ chord_identification_script.js
@@ -1,3 +1,8 @@
 const chordNotes = document.getElementById("chord-notes");
+const chordRootNoteButtons = document.getElementsByName("chord-root-note");
+const chordRootModifierButtons = document.getElementsByName("chord-root-modifiers");
+const chordRangeButtons = document.getElementsByName("chord-range");
+const evaluateBtn = document.getElementById("evaluate-button");
+const feedbackParagraph = document.getElementById("feedback-paragraph");
 
 chordNotes.innerText = Chord.createRandom().toNotesString();
